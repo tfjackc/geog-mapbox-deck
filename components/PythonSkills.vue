ec047a66e30e697330691e8c439c81bf5f90f9ee
tfjackc <pnwest.geo@gmail.com>
2024-01-30 20:52:05 -0800

    seo meta

--- components/PythonSkills.vue
+++ components/PythonSkills.vue
@@ -4,13 +4,7 @@ const theme = useTheme()
 </script>
 
 <template>
-  <div class="rounded
-  overflow-hidden
-  hover:border-stone-50
-  flex flex-col
-  md:flex-row
-  justify-space-between
-  h-full">
+  <div class="rounded overflow-hidden hover:border-stone-50 flex flex-col md:flex-row justify-space-between h-full">
     <div class="px-6 py-4">
       <div class="flex items-center mb-4">
         <Icon name="logos:python" color="white" size="36" class="m-1"/>
