ab46a835d68df226a67e5197949eae16164c74ae
tfjackc <jack.colpitt@crookcountyor.gov>
2024-01-31 09:53:54 -0800

    education added

--- components/CardComponent.vue
+++ components/CardComponent.vue
@@ -1,6 +1,12 @@
 <template>
   <v-container>
     <v-row>
+      <v-col cols="12">
+        <div class="text-3xl flex justify-center">Skills</div>
+        <hr class="my-4 border-gray-400">
+      </v-col>
+      </v-row>
+    <v-row>
       <v-col cols="12" md="6">
         <WebDev />
       </v-col>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container mx-auto md:px-40">
+    <About />
     <CardComponent />
   </div>
 </template>
