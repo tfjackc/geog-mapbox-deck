77e7874f5a9f3a3d02373dd4d6ebe108d73790b2
tfjackc <jack.colpitt@crookcountyor.gov>
2024-02-01 11:37:19 -0800

    set up pinia store

---
+++ store/data_store.ts
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+
+
+export const useDataStore = defineStore('data_store', {
+    state: () => ({
+        DEFAULT_MAP_PITCH: 30 as number
+    }),
+    getters: {
+
+    },
+    actions: {
+
+    },
+})
