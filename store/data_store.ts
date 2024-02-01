960c3ec12fc4cc8c54e3d057219142b3ebe9acd3
tfjackc <jack.colpitt@crookcountyor.gov>
2024-02-01 11:38:30 -0800

    ready try and get mapping into the data store

--- store/data_store.ts
+++ store/data_store.ts
@@ -3,12 +3,9 @@ import { defineStore } from 'pinia'
 
 export const useDataStore = defineStore('data_store', {
     state: () => ({
-        DEFAULT_MAP_PITCH: 30 as number
     }),
     getters: {
-
     },
     actions: {
-
     },
 })
